7fcf9670acc7a2a416c2f31e78b6556570d2e064
YurongGao <ygao0096@student.monash.edu>
2022-07-30 21:38:59 +0800

    set up toolbar

--- app/src/main/java/com/eatgao/alienclass/MainActivity.java
+++ app/src/main/java/com/eatgao/alienclass/MainActivity.java
@@ -3,12 +3,26 @@ package com.eatgao.alienclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.eatgao.alienclass.databinding.ActivityMainBinding;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setTitleTextAppearance(this, R.style.Toolbar_TitleText);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Timetable");
     }
 }
